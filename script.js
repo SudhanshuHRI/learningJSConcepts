50cba77d38a58fe344c2771a57eca33043ce918d
sudhanshu <salil221254@gmail.com>
2024-12-24 16:09:54 +0530

    more puzzles added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // (function (){
 //     var a = b = 3;
 // })();
@@ -31,8 +30,8 @@
 // console.log(typeof NaN); // it will consider as number
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
-// console.log({}=={}); //false  
+
+// console.log({}=={}); //false
 // var a = {};
 // var b = {};
 // console.log(a===b); //false
@@ -55,3 +54,168 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// var a = {};
+// var b = a;
+
+//console.log(a===b)//true
+
+// var a = {name:"salil"}
+// var b = {name:"salil"}
+
+//console.log(a===b)//false
+
+// var a = [1,2,3,4]
+// var b = [1,2,3,4]
+
+// console.log(a===b)//false
+// console.log(a[0]===b[0])//true because it is checking values of array
+
+// here a object memory location is copied in b so true + if object and array  compare whether it is blank or have values , it will compare memory location .
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// var a;
+// var b;
+
+// console.log(a);//undefined
+// console.log(b);// undefined
+// console.log(a===b)//true
+// console.log(typeof a)// undefined
+
+//  here both values are compared because they both are variables not array or objects. they both are undefined.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function test(){
+//     function foo(){ //1st foo
+//         return 100;
+//     }
+//     return foo();
+
+//     function foo(){ //2nd foo
+//         return 10;
+//     }
+// }
+
+// console.log(test())//10
+
+// here because functions also have hoisting quality. so both foo function go on top . first foo goes up and second foo goes second. then after calling foo() , 2nd foo function overwrite first foo. so output is 10 .
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// test();
+// console.log(test)
+
+// var test = function (){
+//     console.log("test function");
+// }
+
+// test()
+
+// here if we use function in variable , then hoisting not works. if we console test then it will be undefined because of hoisting. same goes for arrow functions.
+// function (){} => this is function declaration
+// var test = fucntion (){} => this is function expression
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// let data = [3, 1, 7, 12, 23, 45];
+
+// console.log(data.sort()); //[1,12,23,3,45,7]  // here it sort the array uncorrectly beacuse sort() function first converts arry's element in string, then sort it according to letter.
+
+// console.log(
+//   data.sort((a, b) => {
+//     return a - b;
+//   })
+// ); //[1,3,7,12,23,45] it will sort array according.
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Laxical Scope => when inner function can access outer function's variable. eg-
+
+// function hello(){
+//     var a = 10;
+
+//     function innerFunction (){
+//         return a;
+//     }
+
+//     return innerFunction();
+// }
+
+// console.log(hello())
+
+// Clouser => it is a inner function who remembers outer function's variables while outer function is excuted fully.eg-
+
+// function hello() {
+//   var a = 10;
+//   function innerFunction() {
+//     return a;
+//   }
+//   return innerFunction;
+// }
+
+// var inner = hello();
+
+// console.log(inner());//10
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// => Crome's v8 engine have 2 parts :- 1.Memeory Heap, 2. Call Stack.
+// => Memeory Heap :- where variable are saved. browser's memeory
+// => Call Stack :- a container in which tasks are stored line wise. it works on FILO(first in last out).
+//=> js is single threaded. means it has single call stack so it can do only one task in one time.
+// => js is single threaded and syncronous by default but it has built in features to handl async operations like callback, Promises, async/await, Event loop.
+//=>syncronise example:-
+// console.log("start")
+// console.log("end")
+
+// => async example:-
+
+// console.log("start");
+// setTimeout(() => console.log("async operation"), 2000);
+// console.log("end");
+
+// => js use event loop and task que to manage asyncronous operation without blocking main thread. it helps in fetching api and file reading
+// => in call stack which task is on top it will excute first.
+
+//*code                                //*WebAPI
+// *task Que/callback que
+//*call stack                  //*event loop
+
+// ==> while excuting call stack container 's task, if any task comes who takes time, it send to callback que container(container that contains all callbacks). tasks stored in  callback que goes to webAPI container to excute(excuted by ajax and DOM) one by one. when task is completed by webAPI then it comes back in callback que. Then event loop pushes that task back in call stack.
+
+// reference links :- 1. https://www.jsv9000.app
+//                     2. https://www.youtube.com/watch?v=knLtKU4XvaU&list=PL8p2I9GklV44pN_8iYi2pPl2Gw4Pwb70f&index=14
+//                     3. https://www.youtube.com/watch?v=knLtKU4XvaU&list=PL8p2I9GklV44pN_8iYi2pPl2Gw4Pwb70f&index=15
+
+///////////////////////////////////////////////// Promises in JS //////////////////////////////////////////////////////////////
+
+// => it is a object which returns a value which can be recived in future. Promise means task will ither solve or reject.
+// => JS is single threaded so it dont wait for function to excute fully when it takes time to excute. if it takes time, js start excuting next code.
+// => fetch() always returns 2 Promieses. so we use 2 times .then and await. it's called "Promise chaining". it can be more then 2 .
+// => then() function takes a call back. it is excuted when Promise is completed(resolve or reject)
+//=> catch() function is used to check the error or Promise rejected.
+//=>finally function is used whether Promise resolve or reject, this function will excute 100%.
+
+let data = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise resolved");
+    //reject("Promiese rejected");
+    // here if we use both so first will excute and second will not.
+  }, 2000);
+});
+
+data
+  .then((item1) => {
+    console.log("item1", item1);
+    return item1;
+  })
+  .then((item2) => {
+    console.log("item2", item2);
+  })
+  .catch((error) => {
+    console.log("error is:", error);
+  })
+  .finally(() => {
+    console.log("finally statement excuted");
+  });
