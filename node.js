2e6f278383b2180217b859b6802be86d56cc656e
sudhanshu <salil221254@gmail.com>
2024-12-25 17:38:12 +0530

    node project added

---
+++ node.js
@@ -0,0 +1,9 @@
+//to make a project of Node.js follow the steps--------------------
+
+// 1. make a project folder.
+// 2. open in vs code
+// 3. make entry file with index.js
+// 4. install package.json with "npm init" in terminal
+// 5.install node modules with "npm install" in terminal
+// 6. add gitignone file in the project.
+// 7. add dependencies as required and add modules and other files as required.
