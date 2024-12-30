cfc68f24d8450ad6aca5207ea6548c3d4e17468e
sudhanshu <salil221254@gmail.com>
2024-12-30 16:28:36 +0530

    updated

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,29 @@
+///////////////////////////////////////////////////// some tasks to practice ///////////////////////////////////////////
+
+// 1. Program to find longest word in a given sentence ?
+//  2. How to check whether a string is palindrome or not ?
+//  3. Write a program to remove duplicates from an array ?
+//  4. Program to find Reverse of a string without using built-in method ?
+//  5. Find the max count of consecutive 1’s in an array ?
+//  6. Find the factorial of given number ?
+//  7. Given 2 arrays that are sorted [0,3,4,31] and [4,6,30]. Merge them and sort [0,3,4,4,6,30,31] ?
+//  8. Create a function which will accepts two arrays arr1 and arr2. The function should return true if every value in arr1 has its corresponding value squared in array2. The frequency of values must be same.
+//  9. Given two strings. Find if one string can be formed by rearranging the letters of other string.
+//  10. Write logic to get unique objects from below array ?
+//  I/P: [{name: "sai"},{name:"Nang"},{name: "sai"},{name:"Nang"},{name: "111111"}];
+//  O/P: [{name: "sai"},{name:"Nang"}{name: "111111"}
+//  11. Write a JavaScript program to find the maximum number in an array.
+//  12. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.
+//  13. Write a JavaScript function to check if a given number is prime.
+//  14. Write a JavaScript program to find the largest element in a nested array.
+//  [[3, 4, 58], [709, 8, 9, [10, 11]], [111, 2]]
+//  15. Write a JavaScript function that returns the Fibonacci sequence up to a given number of terms.
+//  16. Given a string, write a javascript function to count the occurrences of each character in the string.
+//  17. Write a javascript function that sorts an array of numbers in ascending order.
+//  18. Write a javascript function that sorts an array of numbers in descending order.
+//  19. Write a javascript function that reverses the order of words in a sentence without using the built-in reverse() method.
+//  20. Implement a javascript function that flattens a nested array into a single-dimensional array.
+
 //////////////////////////////////////////////// working of JS/////////////////////////////////////////////////////////////////
 
 // => Crome's v8 engine have 2 parts :- 1.Memeory Heap, 2. Call Stack.
@@ -91,6 +117,7 @@
 //   });
 
 /////////////////////////////////////////// Promise.all / Promies.allsettled / Promise.race ///////////////////////////////
+
 //=> Promiese.all :- used when multiple promises is used + when all Promise completed then it will give response + if any reject it will response reject + it will not tell other promises solved or rejected  + it takes array and console's array + eg-
 
 // let data = Promise.all([
@@ -348,11 +375,7 @@
 // const str = "The rain in Spain stays mainly in the plain.";
 // const regex = /in/g;
 
-
-
 // const matches = str.matchAll(regex);
-
-
 
 // for (const match of matches) {
 //   console.log(match);
@@ -364,32 +387,6 @@
 
 // => map vs foreach function :- map function returns new array and foreach function dont return anything.
 // => navigator object is used to get information about browser in js
-
-///////////////////////////////////////////////////// some tasks to practice ///////////////////////////////////////////
-
-// 1. Program to find longest word in a given sentence ?
-//  2. How to check whether a string is palindrome or not ?
-//  3. Write a program to remove duplicates from an array ?
-//  4. Program to find Reverse of a string without using built-in method ?
-//  5. Find the max count of consecutive 1’s in an array ?
-//  6. Find the factorial of given number ?
-//  7. Given 2 arrays that are sorted [0,3,4,31] and [4,6,30]. Merge them and sort [0,3,4,4,6,30,31] ?
-//  8. Create a function which will accepts two arrays arr1 and arr2. The function should return true if every value in arr1 has its corresponding value squared in array2. The frequency of values must be same.
-//  9. Given two strings. Find if one string can be formed by rearranging the letters of other string.
-//  10. Write logic to get unique objects from below array ?
-//  I/P: [{name: "sai"},{name:"Nang"},{name: "sai"},{name:"Nang"},{name: "111111"}];
-//  O/P: [{name: "sai"},{name:"Nang"}{name: "111111"}
-//  11. Write a JavaScript program to find the maximum number in an array.
-//  12. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.
-//  13. Write a JavaScript function to check if a given number is prime.
-//  14. Write a JavaScript program to find the largest element in a nested array.
-//  [[3, 4, 58], [709, 8, 9, [10, 11]], [111, 2]]
-//  15. Write a JavaScript function that returns the Fibonacci sequence up to a given number of terms.
-//  16. Given a string, write a javascript function to count the occurrences of each character in the string.
-//  17. Write a javascript function that sorts an array of numbers in ascending order.
-//  18. Write a javascript function that sorts an array of numbers in descending order.
-//  19. Write a javascript function that reverses the order of words in a sentence without using the built-in reverse() method.
-//  20. Implement a javascript function that flattens a nested array into a single-dimensional array.
 
 /////////////////////////////////////////////////// Puzzles in JS ////////////////////////////////////////////////////////////////////
 // (function (){
