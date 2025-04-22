1b70bd10fd95e0ed457b137508e507d6df333e73
sudhanshu <salil221254@gmail.com>
2025-04-22 17:14:23 +0530

    updated

--- node.js
+++ node.js
@@ -20,8 +20,8 @@
 // process.nextTick() vs setImmidiate()? 
 // setTimeout() vs setIntervel()?  
 // Explain the concept of callback hell and how can it be avoided in Node.js? 
-// How does Node.js manage memory? <=====================
-// What is the role of the garbage collector? 
+// How does Node.js manage memory? 
+// What is the role of the garbage collector? <=====================
 // What is the EventEmitter in Node.js, and how does it work?
 // What are streams in Node.js, and how do they improve performance?
 // How many types of streams? give example code.
